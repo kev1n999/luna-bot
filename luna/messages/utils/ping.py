a81ae5b6c70daf39446a487e6b567d52e40dd7dc
kev1n999 <sopratesta999@gmail.com>
2026-01-10 02:48:33 -0300

    fix: removed a unecessary line of the test message-command

--- luna/messages/utils/ping.py
+++ luna/messages/utils/ping.py
@@ -6,4 +6,3 @@ async def ping_callback(message: discord.Message, *args):
 
 def message_setup(register: MessageCommandRegister):
   register.register_command_message("ping", ping_callback)
-  print(register.get_all_command_messages())
